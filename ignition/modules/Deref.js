f3ff684c0637429a11dded9ee75fc196c39b2997
Shritesh <shritesh.sj@gmail.com>
2024-03-16 23:43:22 +0000

    Qualify for Chillz token

--- ignition/modules/Deref.js
+++ ignition/modules/Deref.js
@@ -1,20 +1,20 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("DeRefModule", (m) => {
-  const initialSupply = ethers.parseEther("1000");
+	const initialSupply = ethers.parseEther("1000");
 
-  const erc20 = m.contract("MyERC20", [initialSupply, "Test Token", "TT"]);
-  const ultraVerifier = m.contract("UltraVerifier");
+	const erc20 = m.contract("MyERC20", [initialSupply, "Fan Token", "FT"]);
+	const ultraVerifier = m.contract("UltraVerifier");
 
-  const deref = m.contract("ReferralSystem", [
-    erc20,
-    ultraVerifier,
-    "0x42FF98C4E85212a5D31358ACbFe76a621b50fC02",
-    "app_staging_64f7baac41050d666259b740f95a01f4",
-    "deref-1",
-  ]);
+	const deref = m.contract("ReferralSystem", [
+		erc20,
+		ultraVerifier,
+		"0x42FF98C4E85212a5D31358ACbFe76a621b50fC02",
+		"app_staging_64f7baac41050d666259b740f95a01f4",
+		"deref-1",
+	]);
 
-  return {
-    deref,
-  };
+	return {
+		deref,
+	};
 });
