b6ad98d3bb84bd0fe9ff81e894db80ffccaa3af5
kourin <kourin.code@gmail.com>
2024-03-16 22:58:27 +0000

    add base sepolia network setting

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-chai-matchers");
+require('dotenv').config()
+
+const PRIVATE_KEYS = (process.env.PRIVATE_KEYS || "").split(",")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    base_sepolia: {
+      url: process.env.BASE_SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEYS,
+    }
+  },
   solidity: {
     version: "0.8.20",
     settings: {
