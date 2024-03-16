2d55ae9c9ae783fe31e0dd9d7247bdd332341d8f
Shritesh <shritesh.sj@gmail.com>
2024-03-16 22:41:16 +0000

    deployed contract

--- ignition/modules/Deref.js
+++ ignition/modules/Deref.js
@@ -2,30 +2,19 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("DeRefModule", (m) => {
 	const initialSupply = ethers.parseEther("1000");
-	const erc20Factory = m.contract("MyERC20", [
-		initialSupply,
-		"Test Token",
-		"TT",
-	]);
-	const erc20Deploy = m.call(erc20Factory, "deploy");
-	const erc20Address = m.readEventArgument(erc20Deploy, "Deployed", "addr");
+	const erc20 = m.contract("MyERC20", [initialSupply, "Test Token", "TT"]);
 
-	const ultraVerifierFactory = m.contract("UltraVerifier");
-	const ultraVerifierDeploy = m.call(ultraVerifierFactory, "deploy");
-	const ultraVerifierAddress = m.readEventArgument(
-		ultraVerifierDeploy,
-		"Deployed",
-		"addr"
-	);
+	const ultraVerifier = m.contract("UltraVerifier");
 
-	const refFactory = m.contract("ReferralSystem", [
-		erc20Address,
-		ultraVerifierAddress,
+	const deref = m.contract("ReferralSystem", [
+		erc20,
+		ultraVerifier,
+		"0x42FF98C4E85212a5D31358ACbFe76a621b50fC02",
+		"app_staging_64f7baac41050d666259b740f95a01f4",
+		"deref-1",
 	]);
-	const refDeploy = m.call(refFactory, "deploy");
-	const address = m.readEventArgument(refDeploy, "Deployed", "addr");
 
 	return {
-		address,
+		deref,
 	};
 });
